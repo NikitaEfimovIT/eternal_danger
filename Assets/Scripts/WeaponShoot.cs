fffe1bb452acd53bd0bc8cffb3c393a2bbde3a34
DarkPrince3738 <darkprince3738@gmail.com>
2024-12-19 19:16:49 +0100

    added shooting, fixed animation and weapon camera

---
+++ Assets/Scripts/WeaponShoot.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class WeaponShoot : MonoBehaviour
+{
+    [Header("Shooting Settings")]
+    public Rigidbody bulletPrefab; 
+    public Transform firePoint;
+    public float bulletForce = 30f; 
+    public float fireRate = 0.2f; 
+
+    private float nextFireTime = 0f; 
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
+        {
+            Shoot(); 
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        Rigidbody rb = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        if (rb != null)
+        {
+            rb.AddForce(firePoint.forward * bulletForce, ForceMode.Impulse);
+        }
+
+        Debug.Log("Fired!");
+    }
+}
