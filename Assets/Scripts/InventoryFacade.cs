3d3657892ce48b8ebf80a93963f2311752ec7438
NikitaEfimov0 <nikitaef2002@gmail.com>
2024-12-24 18:23:46 +0300

    added door, open mechanism, inventory, key

---
+++ Assets/Scripts/InventoryFacade.cs
@@ -0,0 +1,39 @@
+namespace UnityEngine
+{
+    public class InventoryFacade:MonoBehaviour
+    {
+        private Inventory inventory;
+        
+        public static InventoryFacade Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject); // Удаляем дублирующийся объект
+                return;
+            }
+
+            Instance = this; // Сохраняем ссылку на текущий объект
+            DontDestroyOnLoad(gameObject); // Сохраняем между сценами
+
+            // Инициализация инвентаря
+            inventory = new Inventory();
+            if (inventory == null)
+            {
+                Debug.LogError("Inventory не найден на объекте с InventoryFacade!");
+            }
+        }
+        
+        public void AddToInventory(KeyItem item)
+        {
+            inventory.AddItem(item);
+        }
+
+        public KeyItem CheckForItemInInventory(int doorId)
+        {
+            return inventory.FindItem(doorId);
+        }
+
+    }
+}
---
+++ Assets/Wall.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+
+    public int doorId = 1;
+    
+    private bool needToOpen = false;
+    
+    private bool needToClose = false;
+    
+    public GameObject door;
+    
+    public InventoryFacade _facade;
+
+    public float openX = 2.4f;
+
+    public float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _facade = InventoryFacade.Instance;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.door.transform.position.x);
+        
+        if(this.door.transform.position.x >= openX)
+            this.needToOpen = false;
+        
+        if(this.door.transform.position.x == 0)
+            this.needToClose = false;
+        
+        if (needToOpen && this.door.transform.position.x < this.openX)
+        {
+            this.door.transform.position = Vector3.MoveTowards(this.door.transform.position, 
+                new Vector3(this.openX, this.door.transform.position.y, this.door.transform.position.z), this.speed*Time.deltaTime);
+        }
+        
+
+        if (needToClose && this.door.transform.position.x > 0f && !this.needToOpen)
+        {
+            this.door.transform.position = Vector3.MoveTowards(this.door.transform.position, 
+                new Vector3(0f, this.door.transform.position.y, this.door.transform.position.z), this.speed*Time.deltaTime);
+
+        }
+    }
+    
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(collider.gameObject.name);
+        if (collider.gameObject.name == "Player")
+        {
+            KeyItem keyItem = this._facade.CheckForItemInInventory(this.doorId);
+            // _facade.AddToInventory(keyItem);
+            Debug.Log(keyItem);
+            if(keyItem != null)
+                this.needToOpen = true;
+            // Destroy(keyPrefab);
+        }
+        
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.name == "Player")
+        {
+            Debug.Log("Exit");
+            this.needToClose = true;
+        }
+    }
+}
