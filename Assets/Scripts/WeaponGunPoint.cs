095f2e983d7f378171c0ccaa1d7cf223bfa4da7f
artur okseniuk <warriorroq@gmail.com>
2025-01-10 15:36:16 +0100

    fixed animation, changed cross logic

---
+++ Assets/Scripts/WeaponGunPoint.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponGunPoint : MonoBehaviour
+{
+    public RawImage image;
+    public Transform gunPoint;
+    public float distance;
+
+    private void Update()
+    {
+        Vector3 point = gunPoint.position + gunPoint.forward * distance;
+        if(Physics.Raycast(gunPoint.position, gunPoint.forward, out var hitPoint, distance))
+            point = hitPoint.point;
+
+        image.rectTransform.position = Camera.main.WorldToScreenPoint(point);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Physics.Raycast(gunPoint.position, gunPoint.forward, out var hitPoint, distance))
+            Gizmos.DrawLine(gunPoint.position, hitPoint.point);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(hitPoint.point, .1f);
+    }
+}
